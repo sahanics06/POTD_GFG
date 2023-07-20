4b3600b4d331a247d3cf74d7de045cbb3ae34faa
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2023-07-20 17:25:03 +0530

    Create Queue Reversal.cpp

---
+++ Stack Queue/Queue Reversal.cpp
@@ -0,0 +1,56 @@
+/*
+Given a Queue Q containing N elements. The task is to reverse the Queue. Your task is to complete the function rev(), that reverses the N elements of the queue.
+
+Example 1:
+
+Input:
+6
+4 3 1 10 2 6
+Output: 
+6 2 10 1 3 4
+Explanation: 
+After reversing the given elements of the queue , the resultant queue will be 6 2 10 1 3 4.
+Example 2:
+
+Input:
+4
+4 3 2 1 
+Output: 
+1 2 3 4
+Explanation: 
+After reversing the given elements of the queue , the resultant queue will be 1 2 3 4.
+Your Task: You need to complete the function rev that takes a queue as parameter and returns the reversed queue. The printing is done automatically by the driver code.
+
+Expected Time Complexity : O(n)
+Expected Auxilliary Space : O(n)
+
+Constraints:
+1 ≤ N ≤ 105
+1 ≤ elements of Queue ≤ 105
+  */
+
+// Solution
+
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+        // add code here.
+        stack<int>tmp;
+        while(!q.empty())
+        {
+            int t=q.front();
+            tmp.push(t);
+            q.pop();
+        }
+        while(!tmp.empty())
+        {
+            int t=tmp.top();
+            q.push(t);
+            tmp.pop();
+        }
+        return q;
+        
+    }
+};
