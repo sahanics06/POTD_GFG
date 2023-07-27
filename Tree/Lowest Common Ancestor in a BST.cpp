8249628a88e1106d257885814f5dd145f69e8e7d
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2023-07-28 00:53:53 +0530

    Update Lowest Common Ancestor in a BST.cpp

--- Tree/Lowest Common Ancestor in a BST.cpp
+++ Tree/Lowest Common Ancestor in a BST.cpp
@@ -36,6 +36,20 @@ Constraints:
 
 class Solution{
     public:
+        // Node* LCA(Node *root, int n1, int n2) // LESS CODE
+        // {
+        //     // code here
+        //     if(root==NULL || root->data==n1 || root->data==n2)
+        //         return root;
+        //     Node* left=LCA(root->left, n1, n2);
+        //     Node* right=LCA(root->right, n1, n2);
+        //     if(left==NULL)
+        //         return right;
+        //     else if(right==NULL)
+        //         return left;
+        //     else
+        //         return root;
+        // }
         Node* LCA(Node *root, int n1, int n2)
         {
             // code here
