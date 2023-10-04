31ebb1c044ca67d0fde68c56f3d1aff7f6ba5059
sahanics06 <86726188+sahanics06@users.noreply.github.com>
2023-10-04 14:42:37 +0530

    Create Column name from a given column number.cpp

---
+++ Maths/Column name from a given column number.cpp
@@ -0,0 +1,52 @@
+/*
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+Excel columns has a pattern like A, B, C, … ,Z, AA, AB, AC,…. ,AZ, BA, BB, … ZZ, AAA, AAB ….. etc. In other words, column 1 is 
+named as “A”, column 2 as “B”, column 27 as “AA” and so on.
+
+Example 1:
+
+Input:
+N = 28
+Output: AB
+Explanation: 1 to 26 are A to Z.
+Then, 27 is AA and 28 = AB.
+
+Example 2:
+
+Input: 
+N = 13
+Output: M
+Explanation: M is the 13th character of
+alphabet.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function colName() which takes the column number N as 
+input and returns the column name represented as a string.
+Expected Time Complexity: O(LogN).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= N <= 1018
+
+*/
+
+//*************************************************************************************************************************
+
+// Solution
+
+// find (n-1)%26 and divide (n-1) by 26. We take n-1 because we are adding the number with 'A'.
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        string ans="";
+        while(n)
+        {
+            int r=(n-1)%26;
+            ans.push_back('A'+r);
+            n=(n-1)/26;
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
